5ad4bbc81af658ea04fe1345bcb45503d40b190b
Lee Peterson <lee.peterson@gmail.com>
2013-01-11 05:51:59 -0800

    Develop Activity History. Set up Stupid Tables.

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -68,63 +68,16 @@ function OnError(request, status, error)
   alert('error');
 }
 
-$(document).ready(function() {
-  jQuery.support.cors = true;
+var table = $('table').stupidtable({
+    // Sort functions here
+});
 
-  $('#hero').orbit({
-    animation: 'fade',
-    animationSpeed: 400,
-    timer: true,
-    advanceSpeed: 8000,
-    captions: false,
-    bullets: true,
-    fluid: '16x7'
-  });
+table.bind('aftertablesort', function (event, data) {
+    // data.column - the index of the column sorted after a click
+    // data.direction - the sorting direction (either asc or desc)
 
-  $('#testimonials').orbit({
-    animation: 'horizontal-slide',
-    animationSpeed: 400,
-    timer: false,
-    advanceSpeed: 8000,
-    captions: false,
-    bullets: true,
-    fluid: '16x8'
-  });
-
-  $('#customer-logos, #partner-logos').orbit({
-    animation: 'horizontal-slide',
-    animationSpeed: 400,
-    timer: false,
-    advanceSpeed: 8000,
-    captions: false,
-    bullets: false
-  });
-
-  $('.click-share').tabSlideOut({
-      tabHandle: '.handle',
-      pathToTabImage: '/images/click-share.png',
-      imageHeight: '218px',
-      imageWidth: '50px',
-      tabLocation: 'right',
-      speed: 300,
-      action: 'click',
-      topPos: '200px',
-      fixedPosition: true
-  });
-
-  $('.launch-now').click(function() {
-    $('.login-required').reveal({
-      animation: 'fade',
-      animationSpeed: 200,
-      closeOnBackgroundClick: false
-    });
-  });
-  
-  $('.launch-email').click(function() {
-    $('.credits-required').reveal({
-      animation: 'fade',
-      animationSpeed: 200,
-      closeOnBackgroundClick: false
-    });
-  });
+    var th = $(this).find("th");
+    th.find(".arrow").remove();
+    var arrow = data.direction === "asc" ? "↑" : "↓";
+    th.eq(data.column).append('<span class="arrow">' + arrow +'</span>');
 });
---
+++ javascripts/public.js
@@ -0,0 +1,63 @@
+var $ = jQuery.noConflict();
+
+$(document).ready(function() {
+  jQuery.support.cors = true;
+
+  $('#hero').orbit({
+    animation: 'fade',
+    animationSpeed: 400,
+    timer: true,
+    advanceSpeed: 8000,
+    captions: false,
+    bullets: true,
+    fluid: '16x7'
+  });
+
+  $('#testimonials').orbit({
+    animation: 'horizontal-slide',
+    animationSpeed: 400,
+    timer: false,
+    advanceSpeed: 8000,
+    captions: false,
+    bullets: true,
+    fluid: '16x8'
+  });
+
+  $('#customer-logos, #partner-logos').orbit({
+    animation: 'horizontal-slide',
+    animationSpeed: 400,
+    timer: false,
+    advanceSpeed: 8000,
+    captions: false,
+    bullets: false
+  });
+
+  $('.click-share').tabSlideOut({
+      tabHandle: '.handle',
+      pathToTabImage: '/images/click-share.png',
+      imageHeight: '218px',
+      imageWidth: '50px',
+      tabLocation: 'right',
+      speed: 300,
+      action: 'click',
+      topPos: '200px',
+      fixedPosition: true
+  });
+
+  $('.launch-now').click(function() {
+    $('.login-required').reveal({
+      animation: 'fade',
+      animationSpeed: 200,
+      closeOnBackgroundClick: false
+    });
+  });
+  
+  $('.launch-email').click(function() {
+    $('.credits-required').reveal({
+      animation: 'fade',
+      animationSpeed: 200,
+      closeOnBackgroundClick: false
+    });
+  });
+  
+});
